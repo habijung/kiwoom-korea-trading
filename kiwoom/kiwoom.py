f42c1e41a99f41e637f6650f97741256cca760fd
habi <habijung0@gmail.com>
2023-03-16 00:48:02 +0900

    feat: PyQt5를 통해서 API 연결하고 로그인 프로그램 실행

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,3 +1,24 @@
-class Kiwoom:
+from PyQt5.QAxContainer import *
+
+
+class Kiwoom(QAxWidget):
     def __init__(self):
+        super().__init__()
+
         print("Class Kiwoom")
+
+        self.get_ocx_instance()
+        self.event_slots()
+        self.signal_login_commConnect()
+
+    def get_ocx_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot)
+
+    def login_slot(self, errCode):
+        print(errCode)
+
+    def signal_login_commConnect(self):
+        self.dynamicCall("CommConnect()")
