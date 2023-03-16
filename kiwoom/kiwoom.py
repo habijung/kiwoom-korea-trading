8a1cb375aa6800f870938280a254339d844ff70c
habi <habijung0@gmail.com>
2023-03-17 00:15:35 +0900

    feat: 예수금과 출금 가능 금액 요청하고 출력

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -21,12 +21,14 @@ class Kiwoom(QAxWidget):
 
         self.signal_login_commConnect()
         self.get_account_info()
+        self.detail_account_info()  # 예수금 요청
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
     def event_slots(self):
         self.OnEventConnect.connect(self.login_slot)
+        self.OnReceiveTrData.connect(self.trdata_slot)
 
     def login_slot(self, errCode):
         print(errors(errCode))
@@ -41,8 +43,51 @@ class Kiwoom(QAxWidget):
 
     def get_account_info(self):
         account_list = self.dynamicCall("GetLoginInfo(String)", "ACCNO")
-        account_num = account_list.split(";")[0]
+        self.account_num = account_list.split(";")[0]
 
-        print(f"Account: {account_num}")
+        print(f"Account: {self.account_num}")
 
-        return account_num
+        return self.account_num
+
+    def detail_account_info(self):
+        print("예수금 요청")
+
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
+
+        # RQName은 마음대로 작성해도 됨
+        self.dynamicCall(
+            "CommRqData(String, String, int, String)",
+            "예수금상세현황요청",
+            "opw00001",
+            "0",
+            "2000",
+        )
+
+    def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
+        """
+        TR 요청을 받는 구역
+        :param sScrNo:
+        :param sRQName:
+        :param sTrCode:
+        :param sRecordName:
+        :param sPrevNext: 다음 페이지가 있는지
+        :return:
+        """
+
+        if sRQName == "예수금상세현황요청":
+            deposit = self.dynamicCall(
+                "GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "예수금"
+            )
+            print(f"예수금: {int(deposit)}")
+
+            ok_deposit = self.dynamicCall(
+                "GetCommData(String, String, int, String)",
+                sTrCode,
+                sRQName,
+                0,
+                "출금가능금액",
+            )
+            print(f"출금가능금액: {int(ok_deposit)}")
